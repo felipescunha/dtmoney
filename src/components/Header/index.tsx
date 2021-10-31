2796c7ec4de209b467e2fd1fa037908e8f9ecfea
felipescunha <fcunha.ti@gmail.com>
2021-10-31 18:38:55 -0300

    Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './style';
+
+interface HeaderProps {
+  onOpenNewTransactionalModal: () => void;
+}
+
+export function Header({onOpenNewTransactionalModal}: HeaderProps){
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionalModal}>
+          Nova transação
+        </button>
+       
+      </Content>
+    </Container>
+  )
+}
