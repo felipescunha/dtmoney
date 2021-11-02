84ad6be9639ed923e0b6b96a1897a86b7a208d82
fcunha <fcunha@fcunhas-imac.home>
2021-11-02 09:04:09 -0300

    background color change

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  background: var(--blue);
+  background: var(--purple);
 `;
 
 export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
-  
+
   padding: 2rem 1rem 10rem;
   display: flex;
   align-items: center;
@@ -16,14 +16,14 @@ export const Content = styled.div`
   button {
     font-size: 1rem;
     color: #fff;
-    background: var(--blue-light);
+    background: var(--purple-light);
     padding: 0 2rem;
     border-radius: 0.25rem;
     height: 3rem;
 
     transition: filter 0.2s;
 
-    outline: none !important;
+    border: none;
 
     &:hover {
       filter: brightness(0.9)
